2c080d60441671d961a0e76aaf3622a9e694a5cf
ggfilfly <semhar264@gmail.com>
2025-06-08 12:59:52 +0700

    Update ping_pong.py
    v3, добавлена обработка события проигрыша

--- ping_pong.py
+++ ping_pong.py
@@ -17,20 +17,26 @@ class Player(GameSprite):
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         
-        if keys[K_DOWN] and self.rect.y < 620:
+        if keys[K_DOWN] and self.rect.y < 370:
             self.rect.y += self.speed
     def update2(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         
-        if keys[K_s] and self.rect.y < 620:
+        if keys[K_s] and self.rect.y < 370:
             self.rect.y += self.speed
 
 ball = GameSprite(275, 225, "ball.png", 10, 50, 50)
 racket1 = Player(520, 200, "платформа.png",10, 150, 50)
 racket2 = Player(30, 200, "платформа.png",10,  150, 50)
 
+speed_x = 3
+speed_y = 3
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render("Player 1 lose!", True, (180, 0, 0))
+lose2 = font1.render("Player 2 lose!", True, (180, 0, 0))
 win_width = 600
 win_height = 500
 back = (56, 125, 119)
@@ -45,12 +51,24 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        if not finish:
-            window.fill(back)
-            ball.draw()
-            racket1.draw()
-            racket2.draw()
-            racket1.update()
-            racket2.update2()
+    if not finish:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        window.fill(back)
+        ball.draw()
+        racket1.draw()
+        racket2.draw()
+        racket1.update()
+        racket2.update2()
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+        if ball.rect.x > 550:
+            finish = True
+            window.blit(lose2, (200, 200))
     display.update()
     clock.tick(60)
